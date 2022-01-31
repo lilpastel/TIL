c3800ddbb06d232a3992fb769e8697eca3c7b9c1
lilpastel <quiz@kakao.com>
2022-01-31 17:27:02 +0900

    ms Rust

---
+++ rust-learning-path/hello-cargo/target/debug/deps/hello_cargo-f8db6cfddf6507e1.d
@@ -0,0 +1,5 @@
+/Users/pastel/github/TIL/rust-learning-path/hello-cargo/target/debug/deps/hello_cargo-f8db6cfddf6507e1: src/main.rs
+
+/Users/pastel/github/TIL/rust-learning-path/hello-cargo/target/debug/deps/hello_cargo-f8db6cfddf6507e1.d: src/main.rs
+
+src/main.rs:
---
+++ rust-learning-path/hello-cargo/target/debug/hello-cargo.d
@@ -0,0 +1,1 @@
+/Users/pastel/github/TIL/rust-learning-path/hello-cargo/target/debug/hello-cargo: /Users/pastel/github/TIL/rust-learning-path/hello-cargo/src/main.rs
